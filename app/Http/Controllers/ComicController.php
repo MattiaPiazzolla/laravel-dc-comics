e53cd8442b5f2c09e69a1668c93bef329098d796
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-01 21:09:50 +0200

    [UPD] added italian to the error messages

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -48,7 +48,29 @@ class ComicController extends Controller
             'type' => 'required|max:50', 
             'artists' => 'required|string', 
             'writers' => 'required|string', 
-        ]);
+        ],
+        [
+            'title.required' => 'Il titolo è obbligatorio',
+            'title.max' => 'Il titolo può essere lungo massimo 50 caratteri',
+            'description.required' => 'La descrizione è obbligatoria',
+            'description.min' => 'La descrizione deve essere di almeno 10 caratteri',
+            'thumb.required' => 'L\'URL dell\'immagine è obbligatorio',
+            'thumb.url' => 'Inserisci un URL valido per l\'immagine',
+            'price.required' => 'Il prezzo è obbligatorio',
+            'price.numeric' => 'Il prezzo deve essere un numero',
+            'price.min' => 'Il prezzo deve essere almeno 0',
+            'series.required' => 'La serie è obbligatoria',
+            'series.max' => 'Il nome della serie può essere lungo massimo 100 caratteri',
+            'sale_date.required' => 'La data di uscita è obbligatoria',
+            'sale_date.date' => 'Inserisci una data valida',
+            'type.required' => 'Il tipo è obbligatorio',
+            'type.max' => 'Il tipo può essere lungo massimo 50 caratteri',
+            'artists.required' => 'Il campo artisti è obbligatorio',
+            'writers.required' => 'Il campo scrittori è obbligatorio',
+
+        ]
+    
+    );
     
         $form_data = $request->all();
 
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -17,47 +17,55 @@
 
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo:</label>
-                <input type="text" id="title" name="title" class="form-control" required>
+                <input type="text" id="title" name="title" class="form-control" required
+                    value="{{ old('title') }}">
             </div>
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione:</label>
-                <textarea id="description" name="description" class="form-control" required></textarea>
+                <textarea id="description" name="description" class="form-control" required>{{ old('description') }}</textarea>
             </div>
 
             <div class="mb-3">
                 <label for="thumb" class="form-label">Immagine (URL):</label>
-                <input type="url" id="thumb" name="thumb" class="form-control" required>
+                <input type="url" id="thumb" name="thumb" class="form-control" required
+                    value="{{ old('thumb') }}">
             </div>
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo:</label>
-                <input type="text" id="price" name="price" class="form-control" required>
+                <input type="text" id="price" name="price" class="form-control" required
+                    value="{{ old('price') }}">
             </div>
 
             <div class="mb-3">
                 <label for="series" class="form-label">Serie:</label>
-                <input type="text" id="series" name="series" class="form-control" required>
+                <input type="text" id="series" name="series" class="form-control" required
+                    value="{{ old('series') }}">
             </div>
 
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data di uscita:</label>
-                <input type="date" id="sale_date" name="sale_date" class="form-control" required>
+                <input type="date" id="sale_date" name="sale_date" class="form-control" required
+                    value="{{ old('sale_date') }}">
             </div>
 
             <div class="mb-3">
                 <label for="type" class="form-label">Tipo:</label>
-                <input type="text" id="type" name="type" class="form-control" required>
+                <input type="text" id="type" name="type" class="form-control" required
+                    value="{{ old('type') }}">
             </div>
 
             <div class="mb-3">
                 <label for="artists" class="form-label">Artisti:</label>
-                <input type="text" id="artists" name="artists" class="form-control" required>
+                <input type="text" id="artists" name="artists" class="form-control" required
+                    value="{{ old('artists') }}">
             </div>
 
             <div class="mb-3">
                 <label for="writers" class="form-label">Scrittori:</label>
-                <input type="text" id="writers" name="writers" class="form-control" required>
+                <input type="text" id="writers" name="writers" class="form-control" required
+                    value="{{ old('writers') }}">
             </div>
 
             <button type="submit" class="btn btn-primary">Aggiungi Fumetto</button>
